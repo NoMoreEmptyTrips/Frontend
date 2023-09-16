97e44da3eba398e347615c1912ba08804b516ce9
Michael Beutler <michael.beutler@iperka.com>
2023-09-16 13:46:12 +0200

    Added basic dashboard.

--- no-more-empty-trips/src/components/RouteMapDirections/RouteMapDirections.tsx
+++ no-more-empty-trips/src/components/RouteMapDirections/RouteMapDirections.tsx
@@ -1,6 +1,5 @@
 import { GeoJsonLayer, IconLayer } from "@deck.gl/layers/typed";
 import DeckGL from "@deck.gl/react/typed";
-import "mapbox-gl/dist/mapbox-gl.css";
 import { useEffect, useState } from "react";
 import {
   AttributionControl,
@@ -9,6 +8,8 @@ import {
   NavigationControl,
   ScaleControl,
 } from "react-map-gl";
+
+import "mapbox-gl/dist/mapbox-gl.css";
 import "./RouteMapDirections.css";
 
 export type Stop = {
@@ -47,6 +48,7 @@ const accessToken =
   "pk.eyJ1IjoibWljaGFlbGJldXRsZXIiLCJhIjoiY2xrMmliYjZwMGN1djNxbXF6bGU5ejQ1aSJ9.HyTfjoA0G5o3m5_EM4qdVw";
 
 const RouteMapDirections = ({ stops }: { stops: Stop[] }) => {
+  const [routes, setRoutes] = useState<Route[]>([]);
   const [layers, setLayers] = useState<any>([]);
   useEffect(() => {
     fetch(
@@ -58,6 +60,7 @@ const RouteMapDirections = ({ stops }: { stops: Stop[] }) => {
     )
       .then((res) => res.json())
       .then((data) => {
+        setRoutes(data.routes);
         setLayers(
           data.routes.map(
             (r: Route, i: number) =>
@@ -72,64 +75,79 @@ const RouteMapDirections = ({ stops }: { stops: Stop[] }) => {
                 wireframe: true,
                 getLineColor: [0, 128, 255],
                 getFillColor: [0, 128, 255],
-                pickable: true,
+                pickable: false,
               })
           )
         );
       });
-  }, []);
+  }, [stops]);
 
   return (
-    <DeckGL
-      layers={[
-        ...layers,
-        new IconLayer({
-          id: "icon-layer",
-          data: stops.map((s, i) => ({
-            coordinates: s.location_metadata.snapped_coordinate,
-            index: i,
-            type: s.type,
-          })),
-          getIcon: (d) => ({
-            url: svgToDataURL(`<svg width="100" height="100" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="12" cy="12" r="10" fill="#fff" stroke="#0080ff" stroke-width="2"/>
+    <>
+      <DeckGL
+        layers={[
+          ...layers,
+          new IconLayer({
+            id: "icon-layer",
+            data: stops.map((s, i) => ({
+              coordinates: s.location_metadata.snapped_coordinate,
+              index: i,
+              type: s.type,
+            })),
+            getIcon: (d) => ({
+              url: svgToDataURL(`<svg width="100" height="100" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="10" fill="#fff" stroke="#0080ff" stroke-width="1"/>
             <text text-anchor="middle" alignment-baseline="central" fill="#0080ff" class="marker" font-size="0.75em">
               <tspan x="12" dy="16">${
                 d.type === "pickup" ? "🏭" : d.index + 1
               }</tspan>
             </text>
           </svg>`),
-            width: 100,
-            height: 100,
+              width: 100,
+              height: 100,
+            }),
+            sizeScale: 8,
+            pickable: true,
+            getPosition: (d) => d.coordinates,
+            getSize: (d) => 5,
+            getColor: (d) => [Math.sqrt(d.exits), 140, 0],
           }),
-          sizeScale: 8,
-          getPosition: (d) => d.coordinates,
-          getSize: (d) => 5,
-          getColor: (d) => [Math.sqrt(d.exits), 140, 0],
-        }),
-      ]}
-      width={1000}
-      height={500}
-      initialViewState={{
-        longitude: stops[0].location_metadata.snapped_coordinate[0],
-        latitude: stops[0].location_metadata.snapped_coordinate[1],
-        zoom: 8,
-        pitch: 0,
-      }}
-      controller
-    >
-      <Map
-        attributionControl={false}
-        reuseMaps
-        mapboxAccessToken={accessToken}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
+        ]}
+        width={1000}
+        height={500}
+        initialViewState={{
+          longitude: stops[0].location_metadata.snapped_coordinate[0],
+          latitude: stops[0].location_metadata.snapped_coordinate[1],
+          zoom: 8,
+          pitch: 0,
+        }}
+        controller
+        getTooltip={({ object }) =>
+          object && `Type: ${object.type}\nStop: #${object.index + 1}`
+        }
       >
-        <NavigationControl position="top-right" />
-        <FullscreenControl position="top-right" />
-        <AttributionControl position="bottom-right" />
-        <ScaleControl position="bottom-right" />
-      </Map>
-    </DeckGL>
+        <Map
+          attributionControl={false}
+          reuseMaps
+          mapboxAccessToken={accessToken}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+        >
+          <NavigationControl position="top-right" />
+          <FullscreenControl position="top-right" />
+          <AttributionControl position="bottom-right" />
+          <ScaleControl position="bottom-right" />
+        </Map>
+      </DeckGL>
+      <div style={{ position: "absolute", right: 0, zIndex: 9999999 }}>
+        {routes && routes.length === 1 && (
+          <ul>
+            {routes[0].legs.map((leg: any, i: number) => (
+              <li key={i}>{leg.summary}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
   );
 };
 
--- no-more-empty-trips/src/pages/Dashboard.tsx
+++ no-more-empty-trips/src/pages/Dashboard.tsx
@@ -1,8 +1,126 @@
+import { Typography } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Unstable_Grid2";
+import { styled } from "@mui/material/styles";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ComposedChart,
+  Legend,
+  Line,
+  ResponsiveContainer,
+  XAxis,
+  Tooltip,
+  YAxis,
+  Area,
+} from "recharts";
+import mockData from "../dashboard_mock.json";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
 function Dashboard() {
+  const values = [
+    { key: "avg_stops", value: "Average Stops", unit: "" },
+    { key: "dropoffs", value: "Deliveries", unit: "" },
+    { key: "avg_wait_time", value: "Average Wait Time", unit: "s" },
+  ];
+
   return (
-    <>
-      <div>Dashboard</div>
-    </>
+    <Grid container spacing={2}>
+      <Grid xs={12}>
+        <Item>
+          <Typography variant="h6">Overview</Typography>
+          <div style={{ height: 400 }}>
+            <ResponsiveContainer width="100%" height="100%">
+              <ComposedChart
+                width={150}
+                height={40}
+                data={Object.keys(mockData.avg_empty_km).map((r: any) => ({
+                  name: r,
+                  empty: Math.round(
+                    (100 / (mockData.total_odometer as any)[r]) *
+                      Number((mockData.avg_empty_km as any)[r])
+                  ),
+                  loaded: Math.round(
+                    (100 / (mockData.total_odometer as any)[r]) *
+                      Number(
+                        (mockData.total_odometer as any)[r] -
+                          (mockData.avg_empty_km as any)[r]
+                      )
+                  ),
+                  deliveries: Number((mockData.dropoffs as any)[r]),
+                }))}
+              >
+                <Legend />
+                <Tooltip />
+                <YAxis
+                  unit="%"
+                  yAxisId={"km"}
+                  max={100}
+                  min={100}
+                  domain={[0, 100]}
+                />
+                <YAxis orientation="right" yAxisId={"deliveries"} />
+                <XAxis />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Area
+                  dataKey="empty"
+                  yAxisId={"km"}
+                  stackId="a"
+                  fill="#dc2626"
+                  stroke="#dc2626"
+                />
+                <Area
+                  dataKey="loaded"
+                  yAxisId={"km"}
+                  stackId="a"
+                  fill="#65a30d"
+                  stroke="#65a30d"
+                />
+                <Line
+                  type="monotone"
+                  yAxisId={"deliveries"}
+                  dataKey="deliveries"
+                  stroke="#0284c7"
+                />
+              </ComposedChart>
+            </ResponsiveContainer>
+          </div>
+        </Item>
+      </Grid>
+
+      {values.map((k) => (
+        <Grid xs={12} lg={4} key={k.key}>
+          <Item>
+            <Typography variant="h6">{k.value}</Typography>
+            <div style={{ height: 300 }}>
+              <ResponsiveContainer width="100%" height="100%">
+                <BarChart
+                  width={150}
+                  height={40}
+                  data={Object.keys((mockData as any)[k.key]).map((r: any) => ({
+                    name: r,
+                    value: Number(((mockData as any)[k.key] as any)[r]),
+                  }))}
+                >
+                  <YAxis unit={k.unit} /> <Tooltip />
+                  <XAxis />
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <Bar dataKey="value" fill="#0284c7" />
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
+          </Item>
+        </Grid>
+      ))}
+    </Grid>
   );
 }
 
